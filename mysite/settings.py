f8f8f41447325fce26505b3c0dac2db4593ab55b
Rene Onate <lonate12@gmail.com>
2020-08-05 21:47:16 -0700

    Added home page

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from api-token import django_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = django_secret_key
+SECRET_KEY = '82)$ok#b_obyai182e!m^q71f8dl@ngjc#%pr0!66a8s0a@-0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
